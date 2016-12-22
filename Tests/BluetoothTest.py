07590f3a9e7cc93bedfb598304522539696fa1ca
Murray Ireland <murraylireland@gmail.com>
2016-12-22 15:59:23 +0000

    Added to bluetooth gamepad test

--- Tests/BluetoothTest.py
+++ Tests/BluetoothTest.py
@@ -3,4 +3,47 @@
 # 22/12/2016
 __author__ = 'Murray Ireland'
 
+# Load library functions
+import time
+import pygame
+import RPi.GPIO
 
+# Initialise
+pygame.init()
+pygame.joystick.init()
+
+# Number of joysticks available
+joystick_count = pygame.joystick.get_count()
+
+if joystick_count == 0 :
+    print "No controllers available"
+else :
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+
+    # Get the joystick name
+    name = joystick.get_name()
+    print("Joystick name: {}".format(name))
+
+    # Get joystick axes
+    axes = joystick.get_numaxes()
+    print("Number of axes: {}".format(axes))
+
+    # Get joystick buttons
+    buttons = joystick.get_numbuttons()
+    print("Number of buttons: {}".format(buttons))
+
+
+if joystick_count == 1 :
+    while True :
+        # Print axes values
+        for i in range(axes) :
+            axis = joystick.get_axis(i)
+            print("Axis {} value: {:f}".format(i,axis))
+
+        for i in range(buttons) :
+            button = joystick.get_button(i)
+            print("Button {} value {:f}".format(i,button))
+        
+        # Wait
+        time.sleep(1)

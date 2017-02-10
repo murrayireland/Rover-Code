bdbdc313ef9af838d983b2095ce6b9139b79eedd
Murray Ireland <murraylireland@gmail.com>
2017-02-10 11:16:49 +0000

    Updated Lynxmotion controller Removed RasPiRobot module from code and created explicit commands for motor control

--- tests/RasPiRobot/DirectMotorTest.py
+++ tests/RasPiRobot/DirectMotorTest.py
@@ -12,33 +12,37 @@ LEFT_2_PIN = 4
 RIGHT_1_PIN = 10
 RIGHT_2_PIN = 25
 
+# Directions (1 - fwd, 0 - rvrs)
+right_dir = 1
+left_dir = 1
+
 # Initialise PWM
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LEFT_PWM_PIN, GPIO.OUT)
-# GPIO.setup(RIGHT_PWM_PIN, GPIO.OUT)
+GPIO.setup(RIGHT_PWM_PIN, GPIO.OUT)
 GPIO.setup(LEFT_1_PIN, GPIO.OUT)
-GPIO.output(LEFT_1_PIN, 0)
+GPIO.output(LEFT_1_PIN, not left_dir)
 GPIO.setup(LEFT_2_PIN, GPIO.OUT)
-GPIO.output(LEFT_2_PIN, 0)
-# GPIO.setup(RIGHT_1_PIN, GPIO.OUT)
-# GPIO.output(RIGHT_1_PIN, 0)
-# GPIO.setup(RIGHT_2_PIN, GPIO.OUT)
-# GPIO.output(RIGHT_2_PIN, 0)
-pwml = GPIO.PWM(LEFT_PWM_PIN, 50)
+GPIO.output(LEFT_2_PIN, left_dir)
+GPIO.setup(RIGHT_1_PIN, GPIO.OUT)
+GPIO.output(RIGHT_1_PIN, not right_dir)
+GPIO.setup(RIGHT_2_PIN, GPIO.OUT)
+GPIO.output(RIGHT_2_PIN, right_dir)
+pwml = GPIO.PWM(LEFT_PWM_PIN, 500)
 pwml.start(5)
-# pwmr = GPIO.PWM(RIGHT_PWM_PIN, 50)
-# pwmr.start(5)
+pwmr = GPIO.PWM(RIGHT_PWM_PIN, 50)
+pwmr.start(5)
 
-for i in range(5, 50, 5):
+for i in range(5, 80, 5):
     pwml.ChangeDutyCycle(i)
-    # pwmr.ChangeDutyCycle(i)
+    pwmr.ChangeDutyCycle(i)
     print(i)
     time.sleep(0.5)
 
 # Stop PWM
 pwml.stop()
-# pwmr.stop()
+pwmr.stop()
 
 # Release pins and clean up
 GPIO.cleanup()
